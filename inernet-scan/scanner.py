eba8218981591fd186218acd47a5c9c20957391a
kali <kali@asus>
2017-09-04 16:02:55 +0800

    change some filename and add ping modules

--- inernet-scan/scanner.py
+++ inernet-scan/scanner.py
@@ -1,8 +1,21 @@
-import os
-import sys
+import ping
 
 
 class LivePcScan():
+    def __init__(self, target):
+        self.target = target
+
+    def getPing(self):
+        for i in self.target:
+            pinger = ping.Pinger(i)
+            result = pinger.ping()
+            if result:
+                print(i + " Live")
+            else:
+                print(i + " Down")
+
+
+class PortScan():
     def __init__(self, target):
         self.target = target
 
@@ -10,10 +23,6 @@ class LivePcScan():
         pass
 
 
-class PortScan():
-    def __init__(self, arg):
-        super(PortScan, self).__init__()
-        self.arg = arg
-
-    def test():
-        pass
+if __name__ == '__main__':
+    test = LivePcScan(['127.0.0.1', '10.10.100.100', 'baidu.com'])
+    test.getPing()
